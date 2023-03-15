dc31c730cf162c4820face869097a0e02bb1dccd
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-03-15 20:35:07 +0700

    Update JS & CSS

--- Wedding-Invitation/assets/js/params.js
+++ Wedding-Invitation/assets/js/params.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(document.location.search);
 const To = params.get("To");
 const S = params.get("S");
-document.getElementById("to").innerHTML=To.replace(/\+/g, " ")
+document.getElementById("to").innerHTML=To.replace(/\+/g, " ").replace(/\-/g, " ")
 var button = document.getElementById("btn-undangan");
 button.addEventListener("click", function() {
     var hrefValue = "Undangan.html?S=" + S;
