c934a1a846056a571de4849fecb33d253ab6ecda
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-01 20:23:31 +0700

    Update Undangan

---
+++ Wedding-Invitation/assets/js/AturInputWidth.js
@@ -0,0 +1,7 @@
+if (window.innerWidth<480){
+    document.getElementById('nama').style.width="75%"
+    document.getElementById('ucapan').style.width="75%"
+} else {
+    document.getElementById('nama').style.width="30em"
+    document.getElementById('ucapan').style.width="30em"
+}
