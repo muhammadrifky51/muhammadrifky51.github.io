279d10af1fe1f561c86918b17728484f6dc37839
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-02 09:42:44 +0700

    Upd

--- Wedding-Invitation/assets/js/AturInputWidth.js
+++ Wedding-Invitation/assets/js/AturInputWidth.js
@@ -5,3 +5,6 @@ if (window.innerWidth<480){
     document.getElementById('nama').style.width="30em"
     document.getElementById('ucapan').style.width="30em"
 }
+
+document.getElementById('head_id').style.height="100vh"
+document.getElementById('head_id').style.backgroundColor="#B5F2F7"
