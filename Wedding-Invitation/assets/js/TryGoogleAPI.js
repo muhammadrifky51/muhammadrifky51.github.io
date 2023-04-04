2c99cb60b1215a4b1729786cb49407a567d26673
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-04 20:08:51 +0700

    Upd

--- Wedding-Invitation/assets/js/TryGoogleAPI.js
+++ Wedding-Invitation/assets/js/TryGoogleAPI.js
@@ -7,22 +7,32 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/1U_apKzNzsmmV_3DvFxjnI8t7G6
       const namanya=document.createElement("p")
       const delnum=0.5+(0.2*(i-1))
       namanya.setAttribute("class","nama_dan_kehadiran wow fadeInDown")
-      namanya.setAttribute("data-wow-duration","0.5s")
+      namanya.setAttribute("data-wow-duration","1.2s")
       namanya.setAttribute("data-wow-delay",delnum.toString()+"s")
-      namanya.innerText=data.values[i][0]+" "
+      namanya.innerText=data.values[i][0]
 
       const statusnya=document.createElement("a")
       statusnya.innerText="a"
-      if (data.values[i][1]="1"){
+      if (data.values[i][1]="Hadir"){
         statusnya.setAttribute("class","kehadiran1")
-      }
-      else if(data.values[i][1]="2"){
+      } else if (data.values[i][1]="TakHadir"){ 
+        console.log(data.values[i][1])
         statusnya.setAttribute("class","kehadiran2")
-      }
-      else{
+      } else {
         statusnya.setAttribute("class","kehadiran3")
       }
       namanya.appendChild(statusnya)
       kotaknya.appendChild(namanya)
+      
+      const babelnya = document.createElement("li")
+      babelnya.setAttribute("class","bubble wow fadeInDown")
+      babelnya.setAttribute("data-wow-duration","1.2s")
+      babelnya.setAttribute("data-wow-delay",delnum.toString()+"s")
+
+      const pesannya=document.createElement("p")
+      pesannya.innerText=data.values[i][2]
+      babelnya.appendChild(pesannya)
+      kotaknya.appendChild(babelnya)
+
     }
   })
