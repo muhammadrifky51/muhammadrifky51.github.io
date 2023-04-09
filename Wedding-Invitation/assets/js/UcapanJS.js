2c4f3c3e55479ef42097cf8acaa7bb9eb9165a7c
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-09 16:57:17 +0700

    Upd

--- Wedding-Invitation/assets/js/UcapanJS.js
+++ Wedding-Invitation/assets/js/UcapanJS.js
@@ -1,7 +1,36 @@
 const SHEET_ID = '1U_apKzNzsmmV_3DvFxjnI8t7G67ELZisDm5pOIxiLpk';
 const API_KEY = 'AIzaSyBWLiCwazxX-joobuNWWeJ_0_CKCGT77rk';
 const CLIENT_EMAIL = 'googleapispreedsheet@wedding-invitation-test1.iam.gserviceaccount.com';
-const PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCk7/jS+JRMkreW\nq3XJCfbiOWGQd/lgDgViY4BVIvG3q5wfB+jMIWqLvjyy6JpUBzhY6azZQWRf0WOo\noyW3ciRaIQ7ZNVGqNlm7IAenM1eRjp1kbqf5H3wDm2aoXH4jnCX8UmsRzDfw0tCV\naczEewkmb0NOhKy9B9wkBALBSrBufgPu5eDMuvBQSxnJWcEPXCxKBv00Ww3rSvb9\nXdhNisLpuTjUjhyC3ynleF0wr4+cATmrApgYdh0Y+6g7V/qB2bI/iFILVPXDiC3U\ne2JdMo+UlHpZd64gxEYDIdnJKApnW20Oloy6ZpnZD2U8ncF6xUpbmvBhpxlR+m82\nxoUzrdwpAgMBAAECggEADN12dT9EMwJVLVyPpggf0Ja89lOzWQOS9jEQbSOaAtNx\nDKcVaJm5KbADpElaI1CCW8YTUElmWsZ2fWyHWDsgHIMcWUMsTOhrIJpPmoI50/3R\np0gt1DPZXJ8qF1sKkDzBZkBn/1YEbYWGFUdwvEkkBjJSx8y3vBFhQZJqBBlvMFkG\nGVb8CPX3frCpTG+G0OIt/GODa8tLNwfEglSpQGv+DYWJWOM3y088b2NlzI+IE6L+\nhKzV20hBnSFaYDbT+0l9/GP01jQSCSKLtIW/x/BJfKYgaGza+ctMcerUznYtl1Kg\n9eQ5fhMQWA0mEU7hxJB3Sos4UUSg4iPaRy5rTUAoQQKBgQDZ4SbPFzgFemzkhiIE\nmkVIy3/b3xY4eBzD3Qwk3Yf99WNG1oaAzuxv5xr3nKu7665VQkHEM8nzx84KDKf4\nD/c29itH1AJW9tOiAd13qWrQN6XzmiSV8DEQ70gbY+t+lDKv9RVigCkgJRcITTOx\nYnoF7mBRrv63zGDWOxawgpKOwQKBgQDBy4mAwB80fpWMsLGl9rOZoT7YAFeU8zpx\nMeBBGH9LEQoQCzW6g1AubazB0gWV2D8AvuWydy/WiporzcLSyD+AyEyLzrlQqLtK\nNX0ReSCmCEKtAWdefmwe8BeuSvIQF0f/xe5xtQ0L33ohux12Z1hjm17SYWgQp7YI\nyKvIEDQPaQKBgQCGYyiT3YdxYZTe05kR67HGd7aSgKRtCjD74lEGQBoMW5nzbIij\n1H3je9GXXuxSSclKz0ZPZX4wPlJowmusRvS90tLZR/x1dWMN70aR805txZU3jyD3\n1qH9Bwob0uLDPVeSToH1MoWrTRVKyouS4uHO3LxORbjY11f3a5TQJImPQQKBgQCB\nQT7p17o4agFW//bMydmKKyjeKoQ/vWaKXwjCghNMvwWmI8PnLXQKM25ZK7o78io7\nVhefy6zlfDZN5wP9UEBCpdWgHfYt3Qxl93z+bXqgIrxN5bTLKqj0jZBN2IDQyKZl\n6t8Zifc0pw4vdzy0X0Nt1BAPY5ZkHWflPeqNGnmOmQKBgQCoQYCG6GvlS6cVJgF4\nyhRt3j3QT823mXEJr6P96tWg5GTe0WelPQZsfmLF8eoiXP1GQ2J5EWPjTjwHR5FK\n/gtKkUk9NmqLhgus/9zE7MM8lYG7f+8NAQsCSJtF6eCVxh8m8b8g8MYgRX8u/mVJ\ni3XGIVOQROcc1CvsWbLoLJZ0hA==\n-----END PRIVATE KEY-----\n";
+const PRIVATE_KEY = " ";
+// const PRIVATE_KEY = `-----BEGIN PRIVATE KEY-----
+// MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCk7/jS+JRMkreW
+// q3XJCfbiOWGQd/lgDgViY4BVIvG3q5wfB+jMIWqLvjyy6JpUBzhY6azZQWRf0WOo
+// oyW3ciRaIQ7ZNVGqNlm7IAenM1eRjp1kbqf5H3wDm2aoXH4jnCX8UmsRzDfw0tCV
+// aczEewkmb0NOhKy9B9wkBALBSrBufgPu5eDMuvBQSxnJWcEPXCxKBv00Ww3rSvb9
+// XdhNisLpuTjUjhyC3ynleF0wr4+cATmrApgYdh0Y+6g7V/qB2bI/iFILVPXDiC3U
+// e2JdMo+UlHpZd64gxEYDIdnJKApnW20Oloy6ZpnZD2U8ncF6xUpbmvBhpxlR+m82
+// xoUzrdwpAgMBAAECggEADN12dT9EMwJVLVyPpggf0Ja89lOzWQOS9jEQbSOaAtNx
+// DKcVaJm5KbADpElaI1CCW8YTUElmWsZ2fWyHWDsgHIMcWUMsTOhrIJpPmoI50/3R
+// p0gt1DPZXJ8qF1sKkDzBZkBn/1YEbYWGFUdwvEkkBjJSx8y3vBFhQZJqBBlvMFkG
+// GVb8CPX3frCpTG+G0OIt/GODa8tLNwfEglSpQGv+DYWJWOM3y088b2NlzI+IE6L+
+// hKzV20hBnSFaYDbT+0l9/GP01jQSCSKLtIW/x/BJfKYgaGza+ctMcerUznYtl1Kg
+// 9eQ5fhMQWA0mEU7hxJB3Sos4UUSg4iPaRy5rTUAoQQKBgQDZ4SbPFzgFemzkhiIE
+// mkVIy3/b3xY4eBzD3Qwk3Yf99WNG1oaAzuxv5xr3nKu7665VQkHEM8nzx84KDKf4
+// D/c29itH1AJW9tOiAd13qWrQN6XzmiSV8DEQ70gbY+t+lDKv9RVigCkgJRcITTOx
+// YnoF7mBRrv63zGDWOxawgpKOwQKBgQDBy4mAwB80fpWMsLGl9rOZoT7YAFeU8zpx
+// MeBBGH9LEQoQCzW6g1AubazB0gWV2D8AvuWydy/WiporzcLSyD+AyEyLzrlQqLtK
+// NX0ReSCmCEKtAWdefmwe8BeuSvIQF0f/xe5xtQ0L33ohux12Z1hjm17SYWgQp7YI
+// yKvIEDQPaQKBgQCGYyiT3YdxYZTe05kR67HGd7aSgKRtCjD74lEGQBoMW5nzbIij
+// 1H3je9GXXuxSSclKz0ZPZX4wPlJowmusRvS90tLZR/x1dWMN70aR805txZU3jyD3
+// 1qH9Bwob0uLDPVeSToH1MoWrTRVKyouS4uHO3LxORbjY11f3a5TQJImPQQKBgQCB
+// QT7p17o4agFW//bMydmKKyjeKoQ/vWaKXwjCghNMvwWmI8PnLXQKM25ZK7o78io7
+// Vhefy6zlfDZN5wP9UEBCpdWgHfYt3Qxl93z+bXqgIrxN5bTLKqj0jZBN2IDQyKZl
+// 6t8Zifc0pw4vdzy0X0Nt1BAPY5ZkHWflPeqNGnmOmQKBgQCoQYCG6GvlS6cVJgF4
+// yhRt3j3QT823mXEJr6P96tWg5GTe0WelPQZsfmLF8eoiXP1GQ2J5EWPjTjwHR5FK
+// /gtKkUk9NmqLhgus/9zE7MM8lYG7f+8NAQsCSJtF6eCVxh8m8b8g8MYgRX8u/mVJ
+// i3XGIVOQROcc1CvsWbLoLJZ0hA==
+// -----END PRIVATE KEY-----
+// `;
 
 const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Tabelnya:append?valueInputOption=RAW`;
 
@@ -22,11 +51,12 @@ document.querySelector('form').addEventListener('submit', function(e) {
       ]};
 
     console.log(`${generateJwtToken(CLIENT_EMAIL, PRIVATE_KEY)}`)
-
+    /*
     fetch(apiUrl,{
       method:'POST',
       headers: {
         'Authorization': `Bearer ${generateJwtToken(CLIENT_EMAIL, PRIVATE_KEY)}`,
+        //'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnb29nbGVhcGlzcHJlZWRzaGVldEB3ZWRkaW5nLWludml0YXRpb24tdGVzdDEuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzdWIiOiJnb29nbGVhcGlzcHJlZWRzaGVldEB3ZWRkaW5nLWludml0YXRpb24tdGVzdDEuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzY29wZSI6Imh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL2F1dGgvc3ByZWFkc2hlZXRzIiwiYXVkIjoiaHR0cHM6Ly9vYXV0aDIuZ29vZ2xlYXBpcy5jb20vdG9rZW4iLCJleHAiOjE2ODA5Mzg3MDQsImlhdCI6MTY4MDkzNTEwNH0.KP0dOs1aC4BOnMG1UT8xK2trvamHYuOK5g8u9sNzTi0',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(databuatdimasukin)
@@ -35,7 +65,7 @@ document.querySelector('form').addEventListener('submit', function(e) {
     .then(data => console.log(data))
     .catch(error => console.error(error));
     
-    /*
+    
     fetch(`https://sheets.googleapis.com/v4/spreadsheets/1U_apKzNzsmmV_3DvFxjnI8t7G67ELZisDm5pOIxiLpk/values/Sheet1:append?valueInputOption=RAW&key=AIzaSyBWLiCwazxX-joobuNWWeJ_0_CKCGT77rk`, {
       method: 'POST',
       headers: {
@@ -45,7 +75,7 @@ document.querySelector('form').addEventListener('submit', function(e) {
     })
     */
 
-    /*
+    
     gapi.client.sheets.values.append({
       spreadsheetID:"1U_apKzNzsmmV_3DvFxjnI8t7G67ELZisDm5pOIxiLpk",
       range:"Tabelnya",
@@ -55,7 +85,7 @@ document.querySelector('form').addEventListener('submit', function(e) {
     }).then((response)=>{
       console.log(response.result)
     })
-    */
+    
     // Membersihkan form setelah ucapan dikirimkan
     document.getElementById('nama').value = '';
     document.getElementById('ucapan').value = '';
@@ -64,34 +94,42 @@ document.querySelector('form').addEventListener('submit', function(e) {
 
   function generateJwtToken(clientEmail, privateKey) {
     const now = Math.floor(Date.now() / 1000);
-    const HeadTxt = "{ alg: 'HS256', typ: 'JWT' }"
+    const HeadTxt = JSON.stringify({ 'alg': 'HS256', 'typ': 'JWT' })
     //const jwtHeader = getBase64Encoded(JSON.stringify({ alg: 'HS256', typ: 'JWT' }));
-    const jwtHeader = getBase64Encoded(HeadTxt);
-    const ClaimTxt = `{
+    const jwtHeader = getBase64EncodedURL(HeadTxt);
+    //const ClaimTxt = `{'iss': '${clientEmail}', 'sub': '${clientEmail}', 'scope': 'https://www.googleapis.com/auth/spreadsheets', 'aud': 'https://www.googleapis.com/oauth2/v4/token', 'exp': '${now + 3600}','iat': '${now}'}`
+    const jwtClaimset = getBase64EncodedURL(JSON.stringify({
       iss: clientEmail,
-      sub:clientEmail,
+      sub: clientEmail,
       scope: 'https://www.googleapis.com/auth/spreadsheets',
-      aud: 'https://www.googleapis.com/oauth2/v4/token',
-      exp: ${now + 3600},
-      iat: ${now}
-    }`
-    // const jwtClaimset = getBase64Encoded(JSON.stringify({
-    //   iss: clientEmail,
-    //   sub:clientEmail,
-    //   scope: 'https://www.googleapis.com/auth/spreadsheets',
-    //   aud: 'https://www.googleapis.com/oauth2/v4/token',
-    //   exp: now + 3600,
-    //   iat: now
-    // }));
-    const jwtClaimset = getBase64Encoded(ClaimTxt)
-    const jwtSignature =  CryptoJS.HmacSHA256(`${jwtHeader}.${jwtClaimset},${privateKey}`);
-    const Sign64 = CryptoJS.enc.Base64.stringify(jwtSignature)
-    return `${jwtHeader}.${jwtClaimset}.${Sign64}`;
+      //aud: 'https://www.googleapis.com/oauth2/v4/token',
+      aud:'https://oauth2.googleapis.com/token',
+      exp: now + 3600,
+      iat: now
+    }));
+    //const jwtClaimset = getBase64Encoded(ClaimTxt)
+    const jwtSignature =  CryptoJS.HmacSHA256(jwtHeader+"."+jwtClaimset,privateKey);
+    const Sign64 = CryptoJS.enc.Base64url.stringify(jwtSignature)
+    
+    //console.log(HeadTxt)
+    //console.log(jwtHeader)
+    //console.log(ClaimTxt)
+    //console.log(jwtClaimset)
+    //console.log(jwtSignature)
+    //console.log(Sign64)
+    
+    return jwtHeader+"."+jwtClaimset+"."+Sign64;
   }
 
   function getBase64Encoded(rawStr){
-    var wordArray = CryptoJS.enc.Base64.parse(rawStr);
+    var wordArray = CryptoJS.enc.Utf8.parse(rawStr);
     var result = CryptoJS.enc.Base64.stringify(wordArray);
+    return result
+  }
+
+  function getBase64EncodedURL(rawStr){
+    var wordArray = CryptoJS.enc.Utf8.parse(rawStr);
+    var result = CryptoJS.enc.Base64url.stringify(wordArray);
     return result
   }
 
