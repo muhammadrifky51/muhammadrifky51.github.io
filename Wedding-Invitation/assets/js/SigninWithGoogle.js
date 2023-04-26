b8267a812acad56bec41b129fee743551e04aa67
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-26 13:35:49 +0700

    Update SigninWithGoogle.js

--- Wedding-Invitation/assets/js/SigninWithGoogle.js
+++ Wedding-Invitation/assets/js/SigninWithGoogle.js
@@ -4,8 +4,19 @@ function authorize() {
     const S1 = params.get("S");
     const redirectUri = 'https://muhammadrifky51.github.io/Wedding-Invitation/Undangan.html?S='+S1;
     const scope = 'https://www.googleapis.com/auth/drive';
-  
-    const url = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let state = '';
+
+    function generateRandomState(length) {
+    for (let i = 0; i < length; i++) {
+        state += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return state;
+    }
+    const randomState = generateRandomState(50);
+    
+    const url = `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code&state=${randomState}`;
   
     window.location.href = url;
   }
