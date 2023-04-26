473e36c1bd9987d21d7abbddf082400134bb7bc0
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-04-26 13:59:11 +0700

    Upd JS

--- Wedding-Invitation/assets/js/params2.js
+++ Wedding-Invitation/assets/js/params2.js
@@ -9,3 +9,42 @@ else if(S=="2"){
 else{
     document.getElementById("sesi").innerHTML='<b>Masih Rahasia ya Muehehehe</b>'
 }
+
+const code = params.get("code");
+if (code) {
+    const clientId = '425332619627-mmdod0nr7ggqnodk02f18c2jhjgeansf.apps.googleusercontent.com';
+    const clientSecret = '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';
+    const redirectUri = 'https://muhammadrifky51.github.io/Wedding-Invitation/Undangan.html?S='+S;
+    const grantType = 'authorization_code';
+  
+    const url = 'https://oauth2.googleapis.com/token';
+  
+    const data = {
+      code: code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      grant_type: grantType,
+    };
+  
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const accessToken = data.access_token;
+        const expiresIn = data.expires_in;
+        const refreshToken = data.refresh_token;
+  
+        console.log(accessToken)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  } else {
+    console.log("Blm Login")
+  }
