887e5d01f958ad37d4e3097810ba1ef1ddd7b8ba
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-05-22 03:19:31 +0700

    Update SigninWithGoogle.js

--- Wedding-Invitation/assets/js/SigninWithGoogle.js
+++ Wedding-Invitation/assets/js/SigninWithGoogle.js
@@ -29,7 +29,7 @@ function unauthorized(){
 
     //document.location.href = `https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=${window.location.href}`;
     
-    NewWindow=window.open('https://www.google.com/accounts/Logout')
+    var NewWindow=window.open('https://www.google.com/accounts/Logout')
     setTimeout(NewWindow.close(),1000)
     
     //document.location = 'https://www.google.com/accounts/Logout';StartPollingForCompletion();return false;
