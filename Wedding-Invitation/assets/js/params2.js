361e0ec58227f8c588eebd0f4871f6b3b18add1c
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-05-21 19:11:19 +0700

    Upd

--- Wedding-Invitation/assets/js/params2.js
+++ Wedding-Invitation/assets/js/params2.js
@@ -93,6 +93,89 @@ if ((code) && !(sessionStorage.getItem('accessToken'))) {
         formcuapcuap.appendChild(inputnama)
         formcuapcuap.appendChild(brkln)
 
+        const pilihankehadiran=document.createElement("ul")
+        pilihankehadiran.className="ucapan_kehadiran row align-item-start"
+        
+        const pilihanijo=document.createElement("li")
+        pilihanijo.className="ijo"
+
+        const inputijo=document.createElement("input")
+        inputijo.type="radio"
+        inputijo.name="kehadiran"
+        inputijo.id="Hadir"
+        inputijo.value="1"
+        inputijo.required=true
+        pilihanijo.appendChild(inputijo)
+
+        const labelijo=document.createElement("label")
+        labelijo.htmlFor="Hadir"
+        labelijo.innerText="Hadir"
+        pilihanijo.appendChild(labelijo)
+
+        const pilihanmerah=document.createElement("li")
+        pilihanmerah.className="merah"
+
+        const inputmerah=document.createElement("input")
+        inputmerah.type="radio"
+        inputmerah.name="kehadiran"
+        inputmerah.id="TakHadir"
+        inputmerah.value="2"
+        inputmerah.required=true
+        pilihanmerah.appendChild(inputmerah)
+
+        const labelmerah=document.createElement("label")
+        labelmerah.htmlFor="TakHadir"
+        labelmerah.innerText="Tidak Hadir"
+        pilihanmerah.appendChild(labelmerah)
+
+        const pilihankuning=document.createElement("li")
+        pilihankuning.className="kuning"
+
+        const inputkuning=document.createElement("input")
+        inputkuning.type="radio"
+        inputkuning.name="kehadiran"
+        inputkuning.id="Tentative"
+        inputkuning.value="3"
+        inputkuning.required=true
+        pilihankuning.appendChild(inputkuning)
+
+        const labelkuning=document.createElement("label")
+        labelkuning.htmlFor="Tentative"
+        labelkuning.innerText="Belum Tahu"
+        pilihankuning.appendChild(labelkuning)
+
+        pilihankehadiran.appendChild(pilihanijo)
+        pilihankehadiran.appendChild(pilihankuning)
+        pilihankehadiran.appendChild(pilihanmerah)
+
+        formcuapcuap.appendChild(pilihankehadiran)
+        formcuapcuap.appendChild(brkln)
+
+        const labelucapan=document.createElement("label")
+        const bintangnyaucapan=document.createElement("span")
+        labelucapan.htmlFor="ucapan"
+        labelucapan.innerText="Ucapan:"
+        bintangnyaucapan.style="color: red;"
+        bintangnyaucapan.innerText="*"
+        labelucapan.appendChild(bintangnyaucapan)
+        formcuapcuap.appendChild(labelucapan)
+        formcuapcuap.appendChild(brkln)
+
+        const textucapan=document.createElement("textarea")
+        textucapan.type="text"
+        textucapan.id="ucapan"
+        textucapan.name="ucapan"
+        textucapan.rows="5"
+        textucapan.required=true
+        formcuapcuap.appendChild(textucapan)
+        formcuapcuap.appendChild(brkln)
+        formcuapcuap.appendChild(brkln)
+
+        const submitbutton=document.createElement("input")
+        submitbutton.type="submit"
+        submitbutton.value="Kirim"
+        formcuapcuap.appendChild(submitbutton)
+
         //const textsignoutnya=document.getElementById("textsignout")
         //textsignoutnya.innerText="Ganti akun?"
         //textsignoutnya.style="color: #00146b;cursor: pointer;font-size:smaller;"
