23a5e240b8515cce7a1d900feaf50ffb9c56557d
Muhammad Rifky Y <53180898+muhammadrifky51@users.noreply.github.com>
2023-06-12 19:51:11 +0700

    Update TryGoogleAPI.js

--- Wedding-Invitation/assets/js/TryGoogleAPI.js
+++ Wedding-Invitation/assets/js/TryGoogleAPI.js
@@ -48,7 +48,9 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/1V-Xhd-KtT9cPimwQyfD4jyzT6v
       namanya.setAttribute("class","nama_dan_kehadiran wow fadeInDown")
       namanya.setAttribute("data-wow-duration","1.2s")
       namanya.setAttribute("data-wow-delay",delnum.toString()+"s")
+      namanya.innerHTML=`<img src="${data.values[i][0]}" alt="${data.values[i][1]}"></img>${data.values[i][2]}`
       
+      /*
       const fotoprofile= document.createElement("img")
       fotoprofile.setAttribute("class", "profpict")
       fotoprofile.src=data.values[i][0]
@@ -56,6 +58,7 @@ fetch('https://sheets.googleapis.com/v4/spreadsheets/1V-Xhd-KtT9cPimwQyfD4jyzT6v
 
       namanya.appendChild(fotoprofile)
       namanya.innerText=data.values[i][2]
+      */
 
       const statusnya=document.createElement("a")
       statusnya.innerText="a"
